13d43d13cf616a1915e35375bffeaa4156906a1d
Ibrahim <ihussein456@gmail.com>
2025-11-29 17:02:49 +0000

    Fixing metadata

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from 'next/og'
+ 
+// Image metadata
+export const alt = 'Beeb Academy'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+ 
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'black',
+        }}
+      >
+        Beeb Academy
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  )
+}
+
